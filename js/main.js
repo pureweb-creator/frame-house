768e301c064246c10a42921a1d9a0aa0cca0a78a
Roman <roman.semenikhin99@gmail.com>
2021-11-24 18:20:51 +0200

    little update

--- js/main.js
+++ js/main.js
@@ -13,24 +13,47 @@ $(document).ready(function(){
 
     $('.go_to').click( function(){
 		var scroll_el = $(this).attr('href');
-	    if ($(scroll_el).length != 0) {
-	    	$(this).parent().parent().parent().animate({ scrollTop: $(scroll_el).offset().top - 60 }, 1000, $.bez([0.77,0,.175,1])); 
-	    }
+        var top = $(scroll_el).offset().top;
+            if ($(scroll_el).length != 0) {
+                $(this).parent().parent().parent().animate({ scrollTop: top }, 1000, $.bez([0.77,0,.175,1])); 
+            }
 	    return false; // выключаем стандартное действие
 	});
 
+    $(function() {
+  
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+          $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+        
+      });
+
+
+    if($(window).width() > 992)
+        var height = $('.first-screen').height();
+    if($(window).width() < 992)
+        var height = $('#page').height() - 40;
+
     $('#leftBtn,.arrow_left').on('click', function(event){
         event.preventDefault();
+        console.log(height);
 
         $('.side_left').addClass('expanded').removeClass('collapsed');
         $('.side_right').addClass('collapsed').removeClass('expanded');
+
+        $('.side_left').animate({ scrollTop: height }, 1000, $.bez([0.77,0,.175,1])); 
     });
 
     $('#rightBtn,.arrow_right').on('click',function(event){
         event.preventDefault();
+        console.log(height);
 
         $('.side_right').addClass('expanded').removeClass('collapsed');
         $('.side_left').addClass('collapsed').removeClass('expanded');
+
+        $('.side_right').animate({ scrollTop: height }, 1000, $.bez([0.77,0,.175,1])); 
     });
 
     $(window)
